d0c49fbbd3f0294f127d65248de26c9f446d84e6
Elton Jacob <elton.jacob@ufl.edu>
2019-03-01 21:43:38 -0500

    added name

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
+#To start a server in Rubby we need rails server
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
