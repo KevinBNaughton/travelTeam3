b2697b60e13215487b6e78bf7f57c2ec27dfe37f
root <root@DESKTOP-2OB86IF.localdomain>
2019-03-14 10:22:14 -0400

    testing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,7 +15,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w( AP.css )
-=======
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
