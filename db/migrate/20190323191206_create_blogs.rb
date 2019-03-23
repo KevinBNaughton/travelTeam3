aee56e9543f76e4834bbb94f3d7516e04d7fa64b
lucasrgusman <kobz1997@ufl.edu>
2019-03-23 15:27:44 -0400

    Added blog database

---
+++ db/migrate/20190323191206_create_blogs.rb
@@ -0,0 +1,26 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+      t.string :name
+      t.date :startDate
+      t.date :endDate
+      t.integer :overallPrice
+      t.integer :numberAdults
+      t.integer :numberChildren
+      t.string :overallDetails
+      t.integer :flightPrice
+      t.string :flightDetails
+      t.integer :cruisePrice
+      t.string :cruiseDetails
+      t.integer :hotelPrice
+      t.integer :hotelBeds
+      t.string :hotelDetails
+      t.string :specialName
+      t.integer :specialPrice
+      t.string :specialDetails
+      t.string :test
+
+      t.timestamps
+    end
+  end
+end
