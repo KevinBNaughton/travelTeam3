32e38b9d536d6223c1f7ab73dd5efb9e1377be3d
lucasrgusman <kobz1997@ufl.edu>
2019-03-30 15:46:11 -0400

    database update

--- db/migrate/20190323191206_create_blogs.rb
+++ db/migrate/20190323191206_create_blogs.rb
@@ -1,24 +1,39 @@
 class CreateBlogs < ActiveRecord::Migration[5.1]
   def change
     create_table :blogs do |t|
-      t.string :name
+
+#basic info. Every post needs these
+      t.string :tripName
       t.date :startDate
       t.date :endDate
       t.integer :overallPrice
       t.integer :numberAdults
       t.integer :numberChildren
       t.string :overallDetails
+      t.string :origin
+      t.string :destination
+
+#flight info
+      t.boolean :flight
       t.integer :flightPrice
       t.string :flightDetails
+#cruise info
+      t.boolean :cruise
       t.integer :cruisePrice
       t.string :cruiseDetails
+#hotel info
+      t.boolean :hotel
       t.integer :hotelPrice
       t.integer :hotelBeds
       t.string :hotelDetails
+#special deal info
+      t.boolean :special
       t.string :specialName
       t.integer :specialPrice
       t.string :specialDetails
+
       t.boolean :postFacebook
+      t.boolean :package
 
       t.timestamps
     end
