7498f6aaaae6f91e09d466e0b11befb4866df935
Kevin Naughton <kevinbnaughton@gmail.com>
2019-03-31 00:09:51 -0400

    Whole login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-  #get 'users/new'
+  get 'sessions/new'
 
-  #get 'blogs/new'
+  resources :users
+  get 'blogs/new'
 
   root 'initial_page#home'
   get '/home',  to: 'initial_page#home'
   get '/contact',  to: 'initial_page#contact'
   get '/admin',  to: 'initial_page#admin'
   get '/post',   to: 'initial_page#post'
-  #resources: :blog
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :blogs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
